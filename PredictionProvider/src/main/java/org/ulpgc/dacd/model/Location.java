c2c6b1d629b4103351345da08698fc3e0c35772e
Jorge <jorgelorenzo188@gmail.com>
2023-12-08 12:06:49 +0000

    We make the entire Location structure with all its parameters as immutable and private but with getter methods.

--- PredictionProvider/src/main/java/org/ulpgc/dacd/model/Location.java
+++ PredictionProvider/src/main/java/org/ulpgc/dacd/model/Location.java
@@ -1,4 +1,25 @@
 package org.ulpgc.dacd.model;
 
 public class Location {
+    private final String island;
+    private final String latitude;
+    private final String longitude;
+
+    public Location(String island, String latitude, String longitude) {
+        this.island = island;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getIsland() {
+        return island;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
 }
