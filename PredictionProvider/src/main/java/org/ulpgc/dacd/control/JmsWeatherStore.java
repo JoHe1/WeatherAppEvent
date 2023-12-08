514bf9156899ab2c62002c642b42860813f8b69b
Jorge <jorgelorenzo188@gmail.com>
2023-12-08 16:36:18 +0000

    Creation of JmsWeatherStore with exception control, we add an exception control called JmsExceptionConection, so we don't infect the interface.

--- PredictionProvider/src/main/java/org/ulpgc/dacd/control/JmsWeatherStore.java
+++ PredictionProvider/src/main/java/org/ulpgc/dacd/control/JmsWeatherStore.java
@@ -1,8 +1,15 @@
 package org.ulpgc.dacd.control;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.ulpgc.dacd.exceptions.ConnectionException;
+import org.ulpgc.dacd.exceptions.JmsExceptionConection;
 import org.ulpgc.dacd.model.Weather;
 
-import java.sql.Connection;
+import javax.jms.*;
+import java.sql.SQLException;
+import java.time.Instant;
 
 public class JmsWeatherStore implements WeatherStore{
     public final String url = "tcp://localhost:61616";
@@ -10,17 +17,50 @@ public class JmsWeatherStore implements WeatherStore{
 
 
     @Override
-    public Connection connection() {
-        return null;
+    public javax.jms.Connection connection() {
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(url);
+        javax.jms.Connection connection = null;
+        try {
+            connection = connectionFactory.createConnection();
+            connection.start();
+        } catch (JMSException e) {
+            throw new JmsExceptionConection("Error establishing JMS connection",e);
+        }
+        return connection;
     }
 
     @Override
     public void close(Connection connection) {
-
+        try {
+            connection.close();
+        } catch (JMSException e) {
+            throw new JmsExceptionConection("Error closing JMS connection",e);
+        }
     }
 
     @Override
     public void save(Weather weather) {
+        try{
+        Connection connection = connection();
 
+        Session session = connection.createSession(false,
+                Session.AUTO_ACKNOWLEDGE);
+        Topic destination = session.createTopic(subject);
+
+        MessageProducer producer = session.createProducer(destination);
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(Instant.class, new InstantTypeAdapter())
+                .create();
+        String weatherJson = gson.toJson(weather);
+        TextMessage message = session
+                .createTextMessage(weatherJson);
+
+        producer.send(message);
+
+        System.out.println("JCG printing@@ '" + message.getText() + "'");
+        close(connection);
+        }catch (JMSException e) {
+            throw new JmsExceptionConection("Error sending message to JMS",e);
+        }
     }
 }
--- PredictionProvider/src/main/java/org/ulpgc/dacd/control/WeatherStore.java
+++ PredictionProvider/src/main/java/org/ulpgc/dacd/control/WeatherStore.java
@@ -2,10 +2,10 @@ package org.ulpgc.dacd.control;
 
 import org.ulpgc.dacd.model.Weather;
 
-import java.sql.Connection;
+import javax.jms.Connection;
 
 public interface WeatherStore {
-    Connection connection();
+    javax.jms.Connection connection();
     void close(Connection connection);
     void save(Weather weather);
 }
