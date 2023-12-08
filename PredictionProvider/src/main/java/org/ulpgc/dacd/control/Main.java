4dbc33eccdeeaad4038798acc143b5b136169a52
Jorge <jorgelorenzo188@gmail.com>
2023-12-08 12:11:28 +0000

    We create all the control classes that will be the logical part of the application and are the ones that will be in operation.

---
+++ PredictionProvider/src/main/java/org/ulpgc/dacd/control/Main.java
@@ -0,0 +1,13 @@
+package org.ulpgc.dacd.control;
+
+import org.ulpgc.dacd.model.Location;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    static Map<String, Location> mapIslandLocation = new HashMap<>();
+        public static void main(String[] args) {
+
+        }
+}
