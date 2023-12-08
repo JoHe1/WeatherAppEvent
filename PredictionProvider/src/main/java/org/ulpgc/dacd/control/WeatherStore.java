9f92ad423de1e2a1209b1dee7302bbae42eb107e
Jorge <jorgelorenzo188@gmail.com>
2023-12-08 12:14:24 +0000

    Add the methods to the WeatherStore interface

--- PredictionProvider/src/main/java/org/ulpgc/dacd/control/WeatherStore.java
+++ PredictionProvider/src/main/java/org/ulpgc/dacd/control/WeatherStore.java
@@ -1,4 +1,11 @@
 package org.ulpgc.dacd.control;
 
+import org.ulpgc.dacd.model.Weather;
+
+import java.sql.Connection;
+
 public interface WeatherStore {
+    Connection connection();
+    void close(Connection connection);
+    void save(Weather weather);
 }
