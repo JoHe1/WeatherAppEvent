7d37db9bec2ebdfe7642e1b2666e9241192a8175
Jorge <jorgelorenzo188@gmail.com>
2023-12-08 12:43:53 +0000

    We create the code of the Task class to periodically execute everything

--- PredictionProvider/src/main/java/org/ulpgc/dacd/control/Task.java
+++ PredictionProvider/src/main/java/org/ulpgc/dacd/control/Task.java
@@ -1,4 +1,17 @@
 package org.ulpgc.dacd.control;
 
-public class Task {
+import java.util.Date;
+
+public class Task implements Runnable{
+    WeatherController weatherController;
+
+    public Task(WeatherController weatherController) {
+        this.weatherController = weatherController;
+    }
+
+    @Override
+    public void run() {
+        weatherController.execute();
+        System.out.println("La tarea se ejecutó a las " + new Date());
+    }
 }
